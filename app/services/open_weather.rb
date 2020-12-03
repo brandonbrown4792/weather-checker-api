8ddb3d44bee430ef9689a45671e0e6f92c1f4a7c
Brandon Brown <hoobie4792@yahoo.com>
2020-12-03 18:06:33 -0500

    Integrated OpenWeather API into Weather controller

---
+++ app/services/open_weather.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'json'
+
+module OpenWeather
+    class GetWeather
+        @base_url = 'https://api.openweathermap.org/data/2.5/weather'
+
+        def self.by_city(city)
+            params = { :q => city, :appid => ENV['API_KEY']}
+            
+            return get_response(params)
+        end
+
+        def self.by_coords(lat, lon)
+           params = { :lat => lat, :lon => lon, :appid => ENV['API_KEY']}
+
+           return get_response(params)
+        end
+
+        private
+
+        def self.get_response(params)
+            uri = URI(@base_url)
+            uri.query = URI.encode_www_form(params)
+
+            response = Net::HTTP.get_response(uri)
+            return JSON.parse(response.body)
+        end
+    end
+end
